7c5a589834d02fa0fb6c0fbae53712bb494f2fc5
Eric SHI <longwosion@gmail.com>
2014-12-09 18:23:35 +0800

    add requirejs text, css plugins and use them to load mmgrid

--- uliweb_peafowl/library/static/js/config.js
+++ uliweb_peafowl/library/static/js/config.js
@@ -1,11 +1,36 @@
 requirejs.config({
-    "baseUrl": "/static/js/lib",
+    "baseUrl": "/static/js/vendor",
     "paths": {
-        "jquery": '/static/jquery/jquery-1.11.1.min.js',
+        "app"      : '../modules',
+        "jquery"   : '/static/jquery/jquery-1.11.1.min.js',
+        "text"     : 'require/text',
+        "css"      : 'require/css', 
+        "markdown" : 'Markdown.Converter',
+
+        "mmGrid"     : 'mmgrid/mmGrid',
+        "mmTreeGrid" : 'mmgrid/mmTreeGrid'
     },
     "shim": {
-    }
+        "mmGrid": {
+            deps: [
+                'mmgrid/mmPaginator',
+                'mmgrid/scrolling',
+
+                'css!mmgrid/mmGrid',
+                'css!mmgrid/mmPaginator'
+            ],
+            exports: 'jQuery.fn.mmGrid'
+        },
+        "mmTreeGrid": {
+            deps: [
+                "mmgrid/mmGrid", 
+                "css!mmgrid/mmTreeGrid"
+            ],
+            exports: 'jQuery.fn.mmGrid'
+        }
+    },
+    urlArgs: "bust=" +  (new Date()).getTime()
 });
 
 // Load the main app module to start the app
-requirejs(["app/main"]);
+requirejs(["app/main", "markdown"]);
---
+++ uliweb_peafowl/library/static/js/modules/main.js
@@ -0,0 +1,6 @@
+console.log("main")
+
+require(["mmGrid"], function(mmgrid){
+    console.log("mmgrid:")
+    console.log(mmgrid)
+})
